268363ae44bd2ec71b362862bb8e238901608916
berukanjing <altheadacua88@gmail.com>
2026-02-04 23:16:36 +0800

    Initial Commit

---
+++ Author.java
@@ -0,0 +1,12 @@
+public class Author {
+    private String name;
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String identity() {
+        return "I am the Author class. I represent the writer of a book.\n";
+    }
+}
+
